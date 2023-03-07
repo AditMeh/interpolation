8d985097dc636469a65bebf5a23eabb3f789b75e
AditMeh <aditmeh123@gmail.com>
2023-03-07 02:55:44 -0500

    stuff

--- trilinear_interpolation.py
+++ trilinear_interpolation.py
@@ -5,40 +5,49 @@ import numpy as np
 def mesh_to_voxel(inp_stream, pts):
     vol = np.zeros((32, 32, 32))
 
-    pts = []
+    points = []
     for _ in range(pts):
         point = [float(p) for p in f.readline().strip("\n").split(" ")]
-        pts.append(point)
+        points.append(point)
 
-    min = 
-        
+    points = np.stack(points)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    xdata = points[:, 1]
+    ydata = points[:, 0]
+    zdata = points[:, 2]
 
-if __name__ == "__main__":
-    f = open("chair_0635.off", "r")
-    f.readline()
-    pts = int(f.readline().strip("\n").split(" ")[0])
-    vox = mesh_to_voxel(f, pts)
+    ax.scatter3D(xdata, ydata, zdata, cmap='Greens')
+    plt.savefig("thing.png")
+    ax.set_axis_off()        
 
-# # prepare some coordinates
-# x, y, z = np.indices((32, 32, 32))
+# if __name__ == "__main__":
+#     f = open("chair_0635.off", "r")
+#     f.readline()
+#     pts = int(f.readline().strip("\n").split(" ")[0])
+#     vox = mesh_to_voxel(f, pts)
 
-# # draw cuboids in the top left and bottom right corners, and a link between them
+# prepare some coordinates
+x, y, z = np.indices((32, 32, 32))
 
-# cube1 = (x < 3) & (y < 3) & (z < 3)
-# cube2 = (x >= 5) & (y >= 5) & (z >= 5)
-# link = abs(x - y) + abs(y - z) + abs(z - x) <= 2
+# draw cuboids in the top left and bottom right corners, and a link between them
 
-# # combine the objects into a single boolean array
-# voxelarray = cube1 | cube2 | link
+cube1 = (x < 3) & (y < 3) & (z < 3)
+print(cube1.shape)
+cube2 = (x >= 5) & (y >= 5) & (z >= 5)
+link = abs(x - y) + abs(y - z) + abs(z - x) <= 2
 
-# # set the colors of each object
-# colors = np.empty(voxelarray.shape, dtype=object)
-# colors[link] = 'red'
-# colors[cube1] = 'blue'
-# colors[cube2] = 'green'
+# combine the objects into a single boolean array
+voxelarray = cube1 | cube2 | link
 
-# # and plot everything
-# ax = plt.figure().add_subplot(projection='3d')
-# ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
+# set the colors of each object
+colors = np.empty(voxelarray.shape, dtype=object)
+colors[link] = 'red'
+colors[cube1] = 'blue'
+colors[cube2] = 'green'
 
-# plt.show()
+# and plot everything
+ax = plt.figure().add_subplot(projection='3d')
+ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
+
+plt.show()
